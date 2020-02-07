448aa6c906c7c315451a4032fda6e36f3a7153b1
barbac <jjbarbac+git@gmail.com>
2020-02-07 14:34:04 -0600

    Add GET /businesses

--- businesses/tests.py
+++ businesses/tests.py
@@ -1,3 +1,109 @@
-from django.test import TestCase
+import pytest
+import json
+from unittest.mock import MagicMock
+from django.test import TestCase, RequestFactory
+from django import test
+from . import models, views
 
-# Create your tests here.
+
+# These are poor man's fixtures
+# To improve the test suit i would spend more time on this choosing more use
+# cases and moving them to ither django's fixtures or pytest's.
+PARAMETERS = dict(
+    name="Yundt-Flatley",
+    address="1386 Lim Brooks",
+    address2="Suite 517",
+    city="Lake Betsy",
+    state="IA",
+    zip="19416",
+    country="US",
+    phone="4034880719",
+    website="http://www.halvorson.com/",
+    created_at="2012-12-10 16:17:58+00:00",
+)
+SAVED_VALUES = PARAMETERS.copy()
+SAVED_VALUES["uuid"] = "2859d6e0-1cb9-4fe9-bc00-97823a9fa4cb"
+
+PARAMETERS2 = dict(
+    name="Botsford Ltd",
+    address="74883 Hane Prairie",
+    address2="",
+    city="Margrettburgh",
+    state="KS",
+    zip="99840",
+    country="US",
+    phone="2462288476",
+    website="http://bergstrom.org/",
+    created_at="2013-11-19 23:26:13+00:00",
+)
+# 'd083169c-4340-4a07-b390-07d297823efd'
+
+
+def test_Business_get(db):
+    business = models.Business(**SAVED_VALUES)
+    business.save()
+
+    mock_request = MagicMock()
+    mock_request.GET.dict.return_value = {}
+    response_businesses = views.Business().get(mock_request).content.decode("utf-8")
+    response_businesses = json.loads(response_businesses)
+    assert len(response_businesses) == 1  # a single record
+    response_businesses[0].pop("id")
+    assert SAVED_VALUES == response_businesses[0]
+
+
+def test_Business_get_find_by_state(db):
+    business = models.Business(**SAVED_VALUES)
+    business.save()
+
+    mock_request = MagicMock()
+    mock_request.GET.dict.return_value = {"state": PARAMETERS["state"]}
+    response_businesses = views.Business().get(mock_request).content.decode("utf-8")
+    response_businesses = json.loads(response_businesses)
+    assert len(response_businesses) == 1  # a single record
+    response_businesses[0].pop("id")
+    assert SAVED_VALUES == response_businesses[0]
+
+
+def test_Business_get_find_by_address1(db):
+    business = models.Business(**SAVED_VALUES)
+    business.save()
+    business2 = models.Business(**PARAMETERS2)
+    business2.save()
+
+    address = "lim brooks"  # all lower case incomplete address
+    mock_request = MagicMock()
+    mock_request.GET.dict.return_value = {"address": address}
+    response_businesses = views.Business().get(mock_request).content.decode("utf-8")
+    response_businesses = json.loads(response_businesses)
+    assert len(response_businesses) == 1  # a single record
+    response_businesses[0].pop("id")
+    assert SAVED_VALUES == response_businesses[0]
+
+
+def test_Business_get_find_by_address2(db):
+    business = models.Business(**SAVED_VALUES)
+    business.save()
+    business2 = models.Business(**PARAMETERS2)
+    business2.save()
+
+    # 2nd addres
+    address = "Suite 517"
+    mock_request = MagicMock()
+    mock_request.GET.dict.return_value = {"address": address}
+    response_businesses = views.Business().get(mock_request).content.decode("utf-8")
+    response_businesses = json.loads(response_businesses)
+    assert len(response_businesses) == 1  # a single record
+    response_businesses[0].pop("id")
+    assert SAVED_VALUES == response_businesses[0]
+
+
+def test_Business_get_find_by_non_exisiting_state(db):
+    business = models.Business(**SAVED_VALUES)
+    business.save()
+
+    mock_request = MagicMock()
+    mock_request.GET.dict.return_value = {"state": "WRONG STATE"}
+    response_businesses = views.Business().get(mock_request).content.decode("utf-8")
+    response_businesses = json.loads(response_businesses)
+    assert len(response_businesses) == 0  # no records
--- businesses/views.py
+++ businesses/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+import json
+from django.db.models import Q
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.forms import ModelForm
+from django.views.generic import View
+from django.http import HttpResponse
 
-# Create your views here.
+from . import models
+
+
+def serialize(data):
+    return json.dumps(data, ensure_ascii=False, indent=2, default=lambda o: str(o))
+
+
+def json_response(data):
+    return HttpResponse(serialize(data), content_type="application/json")
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class Business(View):
+    def get(self, request, *args, **kwargs):
+        parameters = request.GET.dict()
+        filter_kwargs = {}
+
+        # There's only 3 so i didn't create a new function to avoid repetition.
+        name = parameters.pop("name", None)
+        if name:
+            filter_kwargs["name__icontains"] = name
+
+        city = parameters.pop("city", None)
+        if city:
+            filter_kwargs["city__icontains"] = city
+
+        state = parameters.pop("state", None)
+        if state:
+            filter_kwargs["state__icontains"] = state
+
+        address_value = parameters.pop("address", None)
+        address = []
+        if address_value:
+            address = [
+                Q(address__icontains=address_value)
+                | Q(address2__icontains=address_value)
+            ]
+
+        businesses = list(
+            models.Business.objects.filter(*address, **filter_kwargs).values()
+        )
+        return json_response(businesses)
+
